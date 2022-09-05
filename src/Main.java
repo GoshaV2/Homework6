78999bc1e486f8fbc02368a27f88dd405b87d3df
Тиунов Георгий <34783tgs@mail.ru>
2022-09-05 22:47:20 +0300

    сдал дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("дз 1");
+        System.out.println("# 1");
+        {
+            for(int i=1;i<11;i++){
+                System.out.println(i);
+            }
+        }
+        System.out.println("# 2");
+        {
+            for(int i=10;i>0;i--){
+                System.out.println(i);
+            }
+        }
+        System.out.println("# 3");
+        {
+            for(int i=0;i<18;i++){
+                System.out.println(i);
+            }
+        }
+        System.out.println("# 4");
+        {
+            for(int i=10;i>-11;i--){
+                System.out.println(i);
+            }
+        }
+        System.out.println("дз 2");
+        System.out.println("# 1");
+        {
+            for(int i=1904;i<=2096;i=i+4){
+                System.out.println("Вис. год: "+i);
+            }
+        }
+        System.out.println("# 2");
+        {
+            for(int i=7;i<=98;i=i+7){
+                System.out.println(i);
+            }
+        }
+        System.out.println("# 3");
+        {
+            for(int i=1;i<=512;i=i*2){
+                System.out.println(i);
+            }
+        }
+        System.out.println("Дз 3");
+        System.out.println("# 1");
+        {
+            for(int i=1;i<=12;i++){
+                System.out.println("Месяц"+ i +" , сумма накоплений равна "+ i*29000 +" рублей");
+            }
+        }
+        System.out.println("# 2");
+        {
+            for(int i=1;i<=12;i++){
+                System.out.println("Месяц"+ i +" , сумма накоплений равна "+ i*29000*1.01 +" рублей");
+            }
+        }
     }
 }
